6cf47b8278710e9050d7c888d80dbd58f57258a0
Joona Kytöniemi <kytari00@gmail.com>
2022-01-15 15:42:06 +0200

    visual updates

--- src/Footer.js
+++ src/Footer.js
@@ -3,25 +3,32 @@ import { FormattedMessage } from 'react-intl';
 import './styles/Footer.css';
 
 const Footer = () => (
-    <div className="footer-container">
-        <footer className="footer">
-            <p>©{new Date().getFullYear()} <FormattedMessage id='author.name' defaultMessage=''/>Joona Kytöniemi
-                <br/>
-                <FormattedMessage id='contact' defaultMessage='Contact'/>: kjoona<span>@</span>outlook.com
-            </p>
-            <ul className="footer-links">
-                <li>
-                    <a href="https://github.com/jnkyto">GitHub</a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/kjoona/">LinkedIn</a>
-                </li>
-                <li>
-                    <a href="https://twitter.com/jnkyto">Twitter</a>
-                </li>
-            </ul>
-        </footer>
-    </div>
+    <table className="footer-table">
+        <tbody className='footer-table-body'>
+            <tr>
+                <td id="author-td">
+                    <p>©{new Date().getFullYear()} <FormattedMessage id='author.name' defaultMessage=''/>Joona&nbsp;Kytöniemi</p>
+                </td>
+                <td id="contact-td">
+                    <FormattedMessage id='contact' defaultMessage='Contact'/>: kjoona<span>@</span>outlook.com
+                </td>
+                <td id="sns-links-td">
+                    <ul className="footer-links">
+                        <li>
+                            <a href="https://github.com/jnkyto">GitHub</a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/kjoona/">LinkedIn</a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/jnkyto">Twitter</a>
+                        </li>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    
 )
 
 export default Footer;
