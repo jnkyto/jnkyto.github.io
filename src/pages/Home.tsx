a6af3a2f0e61445ae50d75fb7c0928eafdb18872
Joona Kytöniemi <jnkyto@utu.fi>
2025-11-10 18:36:11 +0200

    Fix mobile after project section removal, update some texts and add new translations

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,5 @@
 import meitsi from "../img/meitsi.webp";
 import {FormattedMessage} from "react-intl";
-import ProjectTable from "../components/Project/ProjectTable";
-import ProjectEntry from "../components/Project/ProjectEntry";
-import marumaru from "../img/marumaru.webp";
-import sc from "../img/sc.webp";
 import { LuClipboardCopy } from "react-icons/lu";
 import React from "react";
 import moment from "moment";
@@ -33,18 +29,18 @@ const Home = () => {
         <div className="main_container">
           <ul className="main_table">
             <li className="west_side">
-              <img src={meitsi} id="meitsi" title="Me, posing for the camera like a big dumbo with Tokyo skyline on the background"
-                   alt="Me, posing for the camera like a big dumbo with Tokyo skyline on the background" loading="lazy"/>
+              <img src={meitsi} id="meitsi" title="Me, back turned to the camera, looking towards Lake Bled in Slovenia"
+                   alt="Me, back turned to the camera, looking towards Lake Bled in Slovenia" loading="lazy"/>
             </li>
             <li className="spacer" />
             <li className="east_side">
               <h1>🗣️&nbsp;&nbsp;<FormattedMessage id='information' defaultMessage="Work in progress"/>&nbsp;&nbsp;ℹ️</h1>
               <div className="code_block">
-                <p>name = "Joona Kytöniemi";</p>
-                <p>age = {moment("20000428", "YYYYMMDD").fromNow().slice(0,2)};</p>
-                <p>location = "Turku, Finland";</p>
-                <p>occupation = ["research assistant", "student"];</p>
-                <p>hobbies = "too many";</p>
+                <p><FormattedMessage id='bio.name' defaultMessage=""/></p>
+                <p><FormattedMessage id='bio.age' defaultMessage=""/><span style={{fontFamily: "Courier"}}>{moment("20000428", "YYYYMMDD").fromNow().slice(0,2)}</span></p>
+                <p><FormattedMessage id='bio.location' defaultMessage=""/></p>
+                <p><FormattedMessage id='bio.occupation' defaultMessage=""/></p>
+                <p><FormattedMessage id='bio.hobbies' defaultMessage=""/></p>
                 <button onClick={() => {
                   navigator.clipboard.writeText(pgp_key).then(r => null)
                 }}>
@@ -58,26 +54,6 @@ const Home = () => {
           </ul>
         </div>
       </div>
-      {/*
-    <div className="divider"/>
-    <div className="bread">
-      <h1><FormattedMessage id='bread.projects' defaultMessage="Projects:"/></h1>
-      <ProjectTable>
-        <ProjectEntry
-          link={"https://marumaru.fi"}
-          image={marumaru}
-          img_alt="Logo of marumaru.fi"
-          headline="marumaru"
-          msg_key="project.marumaru" />
-        <ProjectEntry
-          link={"https://soundcloud.com"}
-          image={sc}
-          img_alt="Logo of Soundcloud"
-          headline="music"
-          msg_key="project.music" />
-      </ProjectTable>
-    </div>
-      */}
   </div>
   )
 }
